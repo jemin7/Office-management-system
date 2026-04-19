b57e0405d4394b214abd7aced9b7a7ffe27dc256
Jemin <jaymindesai237@gmail.com>
2026-04-19 21:09:20 +0530

    feat(employee): update employee routes to include validation and correct method assignments

--- server/src/modules/employees/employee.routes.js
+++ server/src/modules/employees/employee.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import validate from "../../common/middleware/validation.middleware.js";
 import createDto from "./dto/create-employee.dto.js";
+import queryDto from "./dto/employee-query.dto.js";
+import updateDto from "./dto/update-employee.dto.js";
 
 const rounter = Router();
 
-rounter.get("/employees",validate(createDto), controller.create);
-rounter.get("/employees/:id");
-rounter.post("/employees");
-rounter.put("/employees/:id");
-rounter.delete("/employees/:id");
+rounter.post("/employees",validate(createDto), controller.create);
+rounter.get("/employees",validate(queryDto), controller.getall);
+rounter.get("/employees/:id", controller.getone);
+rounter.put("/employees/:id", validate(updateDto), controller.update);
+rounter.delete("/employees/:id", controller.remove);
 
 export default rounter;
